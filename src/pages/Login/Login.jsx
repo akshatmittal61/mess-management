99ba41aa8ebd52264f87127b14a0d1fdaa049722
akshatmittal61 <akshatmittal2506@gmail.com>
2022-04-22 00:41:27 +0530

    started login page

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,106 @@
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import { Link } from "react-router-dom";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockIcon from "@mui/icons-material/Lock";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme();
+
+export default function SignIn() {
+	const [user, setUser] = React.useState({
+		email: "",
+		password: "",
+	});
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setUser({
+			...user,
+			[name]: value,
+		});
+	};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(user);
+		setUser({
+			email: "",
+			password: "",
+		});
+	};
+	return (
+		<ThemeProvider theme={theme}>
+			<Container component="header"></Container>
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<Box
+					sx={{
+						marginTop: 8,
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+					}}
+				>
+					<Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+						<LockIcon />
+					</Avatar>
+					<Typography component="h1" variant="h5">
+						Sign in
+					</Typography>
+					<Box
+						component="form"
+						onSubmit={handleSubmit}
+						noValidate
+						sx={{ mt: 1 }}
+					>
+						<TextField
+							margin="normal"
+							required
+							fullWidth
+							name="email"
+							label="Email Address"
+							type="email"
+							id="email"
+							autoComplete="email"
+							value={user.email}
+							onChange={handleChange}
+							autoFocus
+						/>
+						<TextField
+							margin="normal"
+							required
+							fullWidth
+							name="password"
+							label="Password"
+							type="password"
+							id="password"
+							autoComplete="current-password"
+							value={user.password}
+							onChange={handleChange}
+						/>
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							sx={{ mt: 3, mb: 2 }}
+						>
+							Sign In
+						</Button>
+						<Grid container>
+							<Grid item>
+								<Link to="/register">
+									{"Don't have an account? Sign Up"}
+								</Link>
+							</Grid>
+						</Grid>
+					</Box>
+				</Box>
+			</Container>
+		</ThemeProvider>
+	);
+}
