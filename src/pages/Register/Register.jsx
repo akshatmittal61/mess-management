0384a32a2f489be60c20dac9e0886407520c5d7b
akshatmittal61 <akshatmittal2506@gmail.com>
2022-04-22 15:07:47 +0530

    created register route

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,149 @@
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Link, useNavigate } from "react-router-dom";
+import { IconButton } from "@mui/material";
+import { ArrowBack, Lock } from "@mui/icons-material";
+
+const theme = createTheme();
+
+export default function Register() {
+	const navigate = useNavigate();
+	const [user, setUser] = React.useState({
+		fname: "",
+		lname: "",
+		email: "",
+		password: "",
+	});
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setUser({
+			...user,
+			[name]: value,
+		});
+	};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(user);
+		setUser({
+			fname: "",
+			lname: "",
+			email: "",
+			password: "",
+		});
+	};
+
+	return (
+		<ThemeProvider theme={theme}>
+			<Container
+				component="header"
+				sx={{
+					margin: "2rem 0",
+				}}
+			>
+				<IconButton size="large" onClick={() => navigate(-1)}>
+					<ArrowBack />
+				</IconButton>
+			</Container>
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<Box
+					sx={{
+						marginTop: 8,
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+					}}
+				>
+					<Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+						<Lock />
+					</Avatar>
+					<Typography component="h1" variant="h5">
+						Sign up
+					</Typography>
+					<Box
+						component="form"
+						noValidate
+						onSubmit={handleSubmit}
+						sx={{ mt: 3 }}
+					>
+						<Grid container spacing={2}>
+							<Grid item xs={12} sm={6}>
+								<TextField
+									autoComplete="given-name"
+									required
+									fullWidth
+									id="fname"
+									label="First Name"
+									name="fname"
+									value={user.fname}
+									onChange={handleChange}
+									autoFocus
+								/>
+							</Grid>
+							<Grid item xs={12} sm={6}>
+								<TextField
+									required
+									fullWidth
+									id="lname"
+									label="Last Name"
+									name="lname"
+									value={user.lname}
+									onChange={handleChange}
+									autoComplete="family-name"
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									required
+									fullWidth
+									id="email"
+									label="Email Address"
+									name="email"
+									value={user.email}
+									onChange={handleChange}
+									autoComplete="email"
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField
+									required
+									fullWidth
+									name="password"
+									label="Password"
+									type="password"
+									id="password"
+									value={user.password}
+									onChange={handleChange}
+									autoComplete="new-password"
+								/>
+							</Grid>
+						</Grid>
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							sx={{ mt: 3, mb: 2 }}
+						>
+							Sign Up
+						</Button>
+						<Grid container justifyContent="flex-end">
+							<Grid item>
+								<Link to="/login">
+									Already have an account? Sign in
+								</Link>
+							</Grid>
+						</Grid>
+					</Box>
+				</Box>
+			</Container>
+		</ThemeProvider>
+	);
+}
